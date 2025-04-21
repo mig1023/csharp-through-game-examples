7f6e7d10982dd2b99f25b09bd4c7ac196201171b
mig1023 <mig1023@mail.ru>
2025-04-21 14:10:34 +0300

    Command.cs fix

--- Patterns/Command.cs
+++ Patterns/Command.cs
@@ -11,27 +11,33 @@ class Program
     {
         string heroName = String.Empty;
 
-        public Hero(string name) => heroName = name;
+        public Hero(string name) =>
+            heroName = name;
 
-        public void Attack() => Console.WriteLine("\n{0} attack!!", heroName);
+        public void Attack() =>
+            Console.WriteLine("\n{0} attack!!", heroName);
     }
 
     class AttackCommand : IAttack
     {
         Hero hero;
 
-        public AttackCommand(Hero heroSet) => hero = heroSet;
+        public AttackCommand(Hero heroSet) =>
+            hero = heroSet;
 
-        public void Execute() => hero.Attack();
+        public void Execute() =>
+            hero.Attack();
     }
 
     class Fight
     {
         IAttack attack;
 
-        public void SetAttack(IAttack setAttack) => attack = setAttack;
+        public void SetAttack(IAttack setAttack) =>
+            attack = setAttack;
 
-        public void Attack() => attack.Execute();
+        public void Attack() =>
+            attack.Execute();
     }
 
     static void Main()
@@ -39,24 +45,28 @@ class Program
         Console.WriteLine("Welcome to Duelling Club of Hogwarts!\n" +
             "Press 'H' for Harry Potter attack, press 'D' for Draco Malfoy attack...\n");
 
-        Fight fight = new Fight();
+        var fight = new Fight();
 
-        Hero harryPotter = new Hero("Harry Potter");
-        Hero dracoMalfoy = new Hero("Draco Malfoy");
-        
+        var harryPotter = new Hero("Harry Potter");
+        var dracoMalfoy = new Hero("Draco Malfoy");
+
         do
         {
-            ConsoleKeyInfo action = Console.ReadKey();
+            var action = Console.ReadKey();
 
             if (action.Key == ConsoleKey.H)
+            {
                 fight.SetAttack(new AttackCommand(harryPotter));
-
+            }
             else if (action.Key == ConsoleKey.D)
+            {
                 fight.SetAttack(new AttackCommand(dracoMalfoy));
 
+            }
             else if (action.Key == ConsoleKey.Enter)
+            {
                 break;
-
+            }
             else
             {
                 Console.WriteLine("\nI don't know who this is: '{0}'!", action.Key.ToString());
