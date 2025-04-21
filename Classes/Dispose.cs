10a51493be1b012a94f0a64e73ad70862a0abfe9
mig1023 <mig1023@mail.ru>
2025-04-21 14:06:19 +0300

    Dispose.cs fix

--- Classes/Dispose.cs
+++ Classes/Dispose.cs
@@ -6,18 +6,22 @@ namespace csharp_through_code_examples
     {
         private class Gandalf : IDisposable
         {
-            public Gandalf() => Console.WriteLine("[constructor]\t\tGandalf was born in 2500 of the Third Age!");
+            public Gandalf() =>
+                Console.WriteLine("[constructor]\t\tGandalf was born in 2500 of the Third Age!");
 
-            public void Dispose() => Console.WriteLine("[dispose call]\t\tHey, Gandalf, you have to go!");
+            public void Dispose() =>
+                Console.WriteLine("[dispose call]\t\tHey, Gandalf, you have to go!");
 
-            public void OrdinaryMagic() => Console.WriteLine("\t\t\tGandalf does something good!");
+            public void OrdinaryMagic() =>
+                Console.WriteLine("\t\t\tGandalf does something good!");
 
-            ~Gandalf() => Console.WriteLine("[finalizer]\t\tGandalf sailed away at 3200 of the Third Age!");
+            ~Gandalf() =>
+                Console.WriteLine("[finalizer]\t\tGandalf sailed away at 3200 of the Third Age!");
         }
 
         static void GandalfHistory()
         {
-            Gandalf gandalf = new Gandalf();
+            var gandalf = new Gandalf();
 
             while (true)
             {
